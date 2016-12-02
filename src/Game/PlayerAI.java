0d90f4934c2b7513ab703968decb37d2e2702744
marcus <marcus@marcus-AO722>
2016-12-02 13:43:29 -0600

    Updated comments for clarity

--- src/Game/PlayerAI.java
+++ src/Game/PlayerAI.java
@@ -4,14 +4,43 @@ import java.util.*;
 
 public class PlayerAI extends Player{
 	
+	/*
+	 * The PlayerAI utilizes HashMaps to associate a particular domino from
+	 * its hand with a list of spokes that it can play the domino on.
+	 * 
+	 * In the case of scoring, the PlayerAI will reference the scorevalues HashMap
+	 * with respect to the scorable HashMap in order to determine which spoke, when played on,
+	 * will lead to the highest scoring play.
+	 */
+	
+	/**
+	 * A HashMap containing dominoes which can be played on the current board
+	 * and a list of spokes which the domino can be played on
+	 */
 	private HashMap<Domino, List<Integer>> playable;
 	
+	/**
+	 * A HashMap containing dominoes which can be score on the current board
+	 * and a list of spokes which the domino will score on
+	 */
 	private HashMap<Domino, List<Integer>> scorable;
 	
+	/**
+	 * A HashMap containing dominoes which can be score on the current board
+	 * and a list of scores which the domino can make
+	 */
 	private HashMap<Domino, List<Integer>> scorevalues;
 	
+	/**
+	 * The difficulty level of the AIPlayer
+	 * Currently 1 or 2
+	 */
 	private int level;
 	
+	/**
+	 * The highest scoring value possible
+	 * Used to determine which scorable domino to play
+	 */
 	private int highValue;
 	
 	public PlayerAI(String name, int level){
@@ -49,6 +78,10 @@ public class PlayerAI extends Player{
 	private void findMove(Board board){
 		for(Domino d : getHand()){
 			List<Integer> spokes = new ArrayList<Integer>();
+			/*
+			 * openSpokes is used to prevent the PlayerAI from trying to find
+			 * moves on spokes that currently aren't open for play.
+			 */
 			int openSpokes = 2;
 			if(board.getSpokes().get(0).size() > 0 && board.getSpokes().get(1).size() > 0){
 				openSpokes = 4;
@@ -90,14 +123,18 @@ public class PlayerAI extends Player{
 			List<Integer> spokes = new ArrayList<Integer>();
 			List<Integer> values = new ArrayList<Integer>();
 			for(Integer index : playable.get(d)){
-				//Doesn't count the open pip value for spokes 2/3(E/W) if they are empty
+				/*
+				 * Doesn't count the open pip value for spokes 2/3(E/W) if they are empty
+				 */
 				if(index > 1 && board.getSpokes().get(index).size() == 0){
 					cmppip = 0;
 				}
 				else{
 					cmppip = board.getSpokes().get(index).getOpenValue();
 				}
-				//Only the spinner is on the board
+				/*
+				 * Only the spinner is on the board
+				 */
 				if(board.getSpokes().get(0).size() == 0 && board.getSpokes().get(1).size() == 0){
 					//System.out.printf("Only the spinner is on the table. Pip value = %d\tBoard value = %d\n", cmppip, value);
 					if(d.getEndA() == cmppip){
@@ -163,14 +200,20 @@ public class PlayerAI extends Player{
 		List<Domino> playableDominoes = new ArrayList<Domino>(playable.keySet());
 		List<Domino> scorableDominoes = new ArrayList<Domino>(scorable.keySet());
 		List<Domino> highValueDominoes = new ArrayList<Domino>();
-		//Level 1 AI selects move at random, prioritizing scoring moves
+		/*
+		 * Level 1 AI selects move at random, prioritizing scoring moves
+		 */
 		if(level == 1){
-			//Unable to score, just select a playable domino at random
+			/*
+			 * Unable to score, just select a playable domino at random
+			 */
 			if(scorable.isEmpty() && !(playable.isEmpty())){
 				play = playableDominoes.get(random.nextInt(playableDominoes.size()));
 				playIndex = playable.get(play).get(random.nextInt(playable.get(play).size()));
 			}
-			//Score is possible, select random scoring domino
+			/*
+			 * Score is possible, select a scoring domino at random
+			 */
 			else if(!(scorable.isEmpty())){
 				/*for(Domino d : scorable.keySet()){
 					for(Integer value : scorevalues.get(d)){
@@ -198,7 +241,9 @@ public class PlayerAI extends Player{
 		 * domino at random in the event of a tie
 		 */
 		else if(level == 2){
-			//Unable to score, get rid of most valuable domino, still plays on random spoke
+			/*
+			 * Unable to score, get rid of most valuable domino, still plays on random spoke
+			 */
 			if(scorable.isEmpty() && !(playable.isEmpty())){
 				int maxValue = 0;
 				play = playableDominoes.get(0);
@@ -210,7 +255,9 @@ public class PlayerAI extends Player{
 				}
 				playIndex = playable.get(play).get(random.nextInt(playable.get(play).size()));
 			}
-			//Score is possible, select highest scoring domino
+			/*
+			 * Score is possible, select highest scoring domino
+			 */
 			else if(!(scorable.isEmpty())){
 				//int maxValue = 0;
 				for(Domino d : scorable.keySet()){
@@ -246,7 +293,7 @@ public class PlayerAI extends Player{
 			List<Integer> spokes = new ArrayList<Integer>();
 			/*
 			 * Check sides A and B of current Domino D against open spoke values. If a match is found,
-			 * the domino is played 
+			 * the domino is added to the playable HashMap
 			 */
 			for(int i=0; i < board.getSpokes().size(); i++){
 				if(d.getEndA() == board.getSpokes().get(i).getOpenValue()){
@@ -263,15 +310,17 @@ public class PlayerAI extends Player{
 	}
 	
 	/**
-	 * Resets the playable and scorable lists
+	 * Resets the playable and scorable lists.
 	 */
-	
 	private void reset(){
 		playable.clear();
 		scorable.clear();
 		highValue = 0;
 	}
-
+	
+	/**
+	 * Returns a string representation of this class.
+	 */
 	public String toString(){
 		return super.toString() + "\nLevel: " + level +
 					"\nPlayable dominos: " + playable.keySet() + "\nPlayable spokes: " + playable.values()
@@ -288,11 +337,17 @@ public class PlayerAI extends Player{
 		boolean gameOver = false;
 		PlayerAI player = new PlayerAI("AI", 1);
 		PlayerAI player2 = new PlayerAI("AI 2", 2);
-		//Two AI players play against each other
+		/*
+		 * Two AI players play against each other
+		 */
 		while(!gameOver){
 			Boneyard btest = new Boneyard(6);
 			btest.shuffle();
 			Domino spin = new Domino(btest.draw());
+			/*
+			 * Draw from boneyard until a valid double is found, then
+			 * create a new board using that double as the spinner.
+			 */
 			while(!(spin.isDouble())){
 				btest.add(spin);
 				btest.shuffle();
@@ -301,13 +356,20 @@ public class PlayerAI extends Player{
 			Board board = new Board(spin);
 			player.addToHand(btest.drawHand(7));
 			player2.addToHand(btest.drawHand(7));
+			/*
+			 * Play through a round until either player's hand is empty or no more plays are possible
+			 */
 			while(!(player.isHandEmpty() || player2.isHandEmpty())){
 				System.out.printf("----------Round %d Turn %d----------\n", round, turn);
 				System.out.println("Current board state");
 				System.out.println(board.toString());
 				System.out.println("Player 1's turn");
 				System.out.println(player.getHand());
-				player.takeTurn(board, btest);		
+				player.takeTurn(board, btest);
+				/*
+				 * Check if Player 1 has scored and if their score
+				 * is enough to win the game.
+				 */
 				if(board.getBoardValue() > 0 && board.getBoardValue() % 5 ==0){
 					player.addPoints(board.getBoardValue());
 					System.out.printf("Player 1's score is round: %d\ttotal: %d\n", player.getRoundScore(), player.getTotalScore());
@@ -321,7 +383,11 @@ public class PlayerAI extends Player{
 				System.out.println("Player 2's turn");
 				System.out.println(player2.getHand());
 				player2.takeTurn(board, btest);
-				if(board.getBoardValue() > 0 && board.getBoardValue() % 5 ==0){
+				/*
+				 * Check if Player 2 has scored and if their score
+				 * is enough to win the game.
+				 */
+				if(board.getBoardValue() > 0 && board.getBoardValue() % 5 == 0){
 					player2.addPoints(board.getBoardValue());
 					System.out.printf("Player 2's score is round: %d\ttotal: %d\n", player2.getRoundScore(), player2.getTotalScore());
 					if(player2.getTotalScore() >= 250){
@@ -334,11 +400,17 @@ public class PlayerAI extends Player{
 			if(gameOver){
 				break;
 			}
+			/*
+			 * If the round has ended due to a player emptying their hand,
+			 * the other AI player gives their hand value (rounded down to nearest 5)
+			 * to the winning player.
+			 */
 			int handScore = 0;
 			if(player.isHandEmpty()){
 				for(Domino d : player2.getHand()){
 					handScore += d.value();
 				}
+				System.out.printf("Player 1 ends the round\nPlayer 2 hand value: %d\tPoints given: %d\n", handScore, (handScore - (handScore%5)));
 				player.addPoints(handScore - (handScore%5));
 				player2.clearHand();
 			}
@@ -346,6 +418,7 @@ public class PlayerAI extends Player{
 				for(Domino d : player.getHand()){
 					handScore += d.value();
 				}
+				System.out.printf("Player 2 ends the round\nPlayer 1 hand value: %d\tPoints given: %d\n", handScore, (handScore - (handScore%5)));
 				player2.addPoints(handScore - (handScore%5));
 				player.clearHand();
 			}
@@ -353,6 +426,7 @@ public class PlayerAI extends Player{
 			player2.clearRound();
 			player.clearRound();
 			round++;
+			turn = 1;
 		}
 		System.out.printf("Game Over\nFinal Score: Player 1: %d\tPlayer2: %d\n", player.getTotalScore(), player2.getTotalScore());
 	}
