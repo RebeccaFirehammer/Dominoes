9f55e02b09c594afbe6dde10c29852d6e2982c5f
wpg175 <wpg175@T0000B091.cs.utsarr.net>
2016-12-05 14:15:42 -0600

    Update GameInit

--- src/Game/GameInit.java
+++ src/Game/GameInit.java
@@ -10,8 +10,13 @@ import javax.swing.JFrame;
  *  with a user Player and 3 AI Players
  */
 public class GameInit{
+	private boolean noPlay;
+	
+	public boolean noPlay(){
+		return noPlay;
+	}
 	public static void main(String args[]){
-		
+		int turn = 1, round = 1;
 		Boolean gameOver = false;
 		String player;
 		ArrayList<Player> players = new ArrayList<Player>();
@@ -21,7 +26,7 @@ public class GameInit{
 		System.out.println("Name - Player 1: ");
 		player = scan.nextLine();
 		
-		//create 1 user player & 3 AI players
+		//create 1 user player & 3 AI players of each difficulty
 		Player one = new Player(player);
 		Player two = new PlayerAI("CPU 1", 1);
 		Player three = new PlayerAI("CPU 2", 2);
@@ -57,12 +62,61 @@ public class GameInit{
 			two.addToHand(yard.drawHand(7));
 			three.addToHand(yard.drawHand(7));
 			four.addToHand(yard.drawHand(7));
-			System.out.println(players.toString());
 			while(!(one.isHandEmpty() || two.isHandEmpty() || three.isHandEmpty() || four.isHandEmpty())){
 				//play game here
+				System.out.printf("----------Round %d Turn %d----------\n", round, turn);
+				System.out.println("Current board state");
+				System.out.println(board.toString());
+				System.out.println("Player 1's turn");
+				System.out.println(one.getHand());
+				/*
+				 * Check if Player 1 has scored and if their score
+				 * is enough to win the game.
+				 */
+				if(board.getBoardValue() > 0 && board.getBoardValue() % 5 ==0){
+					one.addPoints(board.getBoardValue());
+					System.out.printf("Player 1's score is round: %d\ttotal: %d\n", one.getRoundScore(), one.getTotalScore());
+					if(one.getTotalScore() >= 250){
+						gameOver = true;
+						break;
+					}
+				}
+				System.out.println("Current board state");
+				System.out.println(board.toString());
+				if(one.getHand().isEmpty()){
+					break;
+				}
+				System.out.println("Player 2's turn");
+				System.out.println(two.getHand());
+				two.takeTurn(board, yard);
+				/*
+				 * Check if Player 2 has scored and if their score
+				 * is enough to win the game.
+				 */
+				if(board.getBoardValue() > 0 && board.getBoardValue() % 5 == 0){
+					two.addPoints(board.getBoardValue());
+					System.out.printf("Player 2's score is round: %d\ttotal: %d\n", two.getRoundScore(), two.getTotalScore());
+					if(two.getTotalScore() >= 250){
+						gameOver = true;
+						break;
+					}
+				}
+				/*
+				 * Check to see if both Player 1 and Player 2 are unable to make
+				 * valid plays. 
+				 */
+				if(one.noPlay() && two.noPlay()){
+					System.out.println("Neither player is able to make a move, end of round.");
+					break;
+				}
+				turn++;
+			}
+			if(gameOver){
+				break;
+			}
 			}
 		}
-		
+
 		//run GUI
 		//view.registerListeners(controller);
 		//view.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
